b5f82beee2293ee91e64e849ddb217e6884ff95d
DiegoDiaz <DiegoDiaz@ebroker.es>
2017-12-28 14:48:18 +0100

    system properties

---
+++ repository/src/test/java/com/manning/gia/todo/repository/InMemoryToDoRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.manning.gia.todo.repository;
+
+import com.manning.gia.todo.model.ToDoItem;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class InMemoryToDoRepositoryTest {
+    private ToDoRepository inMemoryToDoRepository;
+
+    @Before
+    public void setUp() {
+        inMemoryToDoRepository = new InMemoryToDoRepository();
+    }
+
+    @Test
+    public void testInsertToDoItem() {
+        String items = System.getProperty("items");
+        assertEquals(items, "21");
+        ToDoItem newToDoItem = new ToDoItem();
+        newToDoItem.setName("Write unit tests");
+        Long newId = inMemoryToDoRepository.insert(newToDoItem);
+        assertNotNull(newId);
+
+        ToDoItem persistedToDoItem = inMemoryToDoRepository.findById(newId);
+        assertNotNull(persistedToDoItem);
+        assertEquals(newToDoItem, persistedToDoItem);
+    }
+
+    @Test
+    public void testOneInsertAdd() {
+        int sizeBefore = inMemoryToDoRepository.findAll().size();
+        ToDoItem newToDoItem = new ToDoItem();
+        newToDoItem.setName("Write unit tests");
+        Long newId = inMemoryToDoRepository.insert(newToDoItem);
+        assertNotNull(newId);
+        int sizeAfter = inMemoryToDoRepository.findAll().size();
+        assertEquals(sizeBefore + 1, sizeAfter);
+    }
+}
